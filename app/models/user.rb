07d22009bf5edc5245ec2d29ed61f78043799e88
marinacor1 <marinaspam1234@gmail.com>
2016-06-08 16:10:16 -0600

    Edit form works

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth_info)
     new_user = User.find_by(github_id: auth_info.extra.raw_info.id)
-    new_user.image = auth_info.info.image
-    new_user.oauth_token = auth_info.credentials.token
-    new_user.first_login = false
-    new_user.save
+    attributes = {image: auth_info.info.image, oauth_token: auth_info.credentials.token, first_login: false}
+    new_user.update_attributes(attributes)
     return new_user
   end
 
--- spec/features/user_can_edit_themselves_spec.rb
+++ spec/features/user_can_edit_themselves_spec.rb
@@ -14,21 +14,21 @@ RSpec.feature "user can edit themselves" do
     expect(current_path).to eq(edit_user_path(user))
 
     within(".edit-user-form") do
-      select("1508")
+      select( "1508", from: "Cohort")
       fill_in "Name", with: "Malcolm Gladwell"
       fill_in "Current Employer", with: "Denny's"
       fill_in "Street Address", with: "1510 Blake Street"
       fill_in "City", with: "Denver"
-      select("CO")
+      select("CO", from: "State")
       fill_in "Email", with: "email@email.com"
       click_on "Submit"
     end
 
     expect(current_path).to eq user_path(user)
     expect(page).to have_content "1508"
-    expect(page).to not_have_content "1602"
+    expect(page).to_not have_content "1602"
     expect(page).to have_content "Malcolm Gladwell"
-    expect(page).to not_have_content "Marina Corona"
+    expect(page).to_not have_content "Marina Corona"
     expect(page).to have_content "Denny's"
     expect(page).to have_content "1510 Blake Street"
     expect(page).to have_content "Denver, CO"
@@ -40,8 +40,8 @@ RSpec.feature "user can edit themselves" do
 
     click_on "Sign in with Github"
 
-    expect(current_path).to eq user_path(user)
-
     expect(user.first_login).to eq(false)
+    
+    expect(current_path).to eq user_path(user)
   end
 end
