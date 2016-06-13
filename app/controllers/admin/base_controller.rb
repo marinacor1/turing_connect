e97ba9fead276485ecfcd018a2bd35034468b629
marinacor1 <marinaspam1234@gmail.com>
2016-06-12 21:44:47 -0600

    User cannot have access to admin page

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,12 @@
 class Admin::BaseController < ApplicationController
-  # before_action :require_admin
-  #
-  # def require_admin
-  #   redirect_to root_path unless current_user.admin?
-  # end
+  before_action :check_guest
+  before_action :require_admin
+
+  def check_guest
+    render file: "/public/404" if current_user.nil?
+  end
+
+  def require_admin
+    render file: "/public/404" unless current_user.admin?
+  end
 end
--- spec/features/user_cannot_edit_other_user_spec.rb
+++ spec/features/user_cannot_edit_other_user_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
-RSpec.feature "user cannot edit another user" do
+RSpec.feature "user does not have admin rights" do
   include FeatureHelper
-  it "returns a flash message and gives 404 error" do
+  it "returns 404 page" do
     user = User.create(cohort: "1508", name: "Sabrina Smith", city: "Miami", state: "FL")
-    user2 = User.create(cohort: "1608", name: "Marina Corona", city: "Los Angeles", state: "CA")
+    user2 = User.create(cohort: "1608", name: "James Larson", city: "Los Angeles", state: "CA")
 
     user_login
 
     visit edit_user_path(user)
 
     expect(page).to have_content "Looks like you are not authorized to access Turing Connect."
+
+    visit admin_users_path
+
+    expect(page).to have_content "Looks like you are not authorized to access Turing Connect."
+
   end
 end
