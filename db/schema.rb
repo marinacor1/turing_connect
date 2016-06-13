03a2812dbe8c5a690370786f9e501ac42f062d98
marinacor1 <marinaspam1234@gmail.com>
2016-06-12 21:47:54 -0600

    Add status column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612213607) do
+ActiveRecord::Schema.define(version: 20160613034608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160612213607) do
     t.float    "latitude"
     t.float    "longitude"
     t.string   "screen_name"
+    t.string   "status"
   end
 
 end
