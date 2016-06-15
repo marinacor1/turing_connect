3b70feb71ad71b826751f6d29efe97d24854df04
marinacor1 <marinaspam1234@gmail.com>
2016-06-14 21:26:56 -0600

    Fixed weird spacing issue with nav

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,5 +1,5 @@
 class UpdatesController < ApplicationController
   def index
-    @updates = Update.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+    @updates = Update.order(created_at: :desc).paginate(page: params[:page], per_page: 8)
   end
 end
