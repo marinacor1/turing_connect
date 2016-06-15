415aca5261a1025da2e09c21db46c735d89c7ed6
marinacor1 <marinaspam1234@gmail.com>
2016-06-15 13:35:59 -0600

    All tests not pending are passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-
   it "returns to full street address" do
     user = User.create(street_address: "1223 Lane Street", city: "San Antonio", state: "TX")
     location = user.full_street_address
@@ -28,6 +27,22 @@ RSpec.describe User, type: :model do
     states = User.unique_cohorts
     expect(states).to eq(["1406", "1410", "1508", "1602"])
   end
+  end
+
+  describe "creates a new update using user information" do
+    fixtures :users
+    xit "has a new update" do
+      params = {"user"=>{"city"=>"New York City"}, "format"=>:json, "controller"=>"api/v1/users", "action"=>"update", "id"=>"1"}
+      original_count = Update.all.count
+
+      new_update = User.update_newsfeed(params)
+      expect(new_update.user.name).to eq("Marina Corona")
+      expect(new_update.cohort).to eq("1602")
+      expect(new_update.action).to eq("updated their city.")
+      expect(Update.all.count).to eq(1)
+
+
+    end
   end
 
 end
