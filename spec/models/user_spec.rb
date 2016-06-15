eceb4821fedf2592a5a61886c463a92fa8c4b821
marinacor1 <marinaspam1234@gmail.com>
2016-06-15 10:31:06 -0600

    Remove uneccessary method in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "converts location to viewable city and state" do
-    user = User.create(city: "San Antonio", state: "TX")
-    location = user.city_and_state(user)
-    expect(location).to eq("San Antonio, TX")
-  end
+
 
   it "returns to full street address" do
     user = User.create(street_address: "1223 Lane Street", city: "San Antonio", state: "TX")
